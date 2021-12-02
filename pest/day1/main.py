9ec6000cd971f3bd60bed8e9f2d70980104f9ddf
Peter Venema <pestilentbox2@gmail.com>
2021-12-01 20:09:07 -0500

    adding day 1 part 1 for pest

---
+++ pest/day1/main.py
@@ -0,0 +1,21 @@
+dir = "pest/day1/"
+#fileName = "inputs.txt"
+fileName = "real_inputs.txt"
+
+def readFromFile(name):
+    with open(name, "r") as file:
+        data = file.readlines()
+        data = [int(x.strip()) for x in data]
+        print(data)
+        return data
+
+def main():
+    total = 0
+    numbers = readFromFile(dir + fileName)
+
+    for a,b in zip(numbers, numbers[1:]):
+        total += 1 if b > a else 0
+
+    print(total)
+
+main()
