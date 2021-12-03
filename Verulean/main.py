0f266c5e5eac944b7e237d6ce588e8d7664517b3
Verulean <65474222+Verulean@users.noreply.github.com>
2021-12-03 10:47:48 -0500

    ok suf

--- Verulean/main.py
+++ Verulean/main.py
@@ -2,8 +2,7 @@ from datetime import datetime as dt
 
 
 def main(n=dt.today().day):
-    exec(f"from aoc{n} import solve", globals())
-    print(solve())
+    print(__import__(f"aoc{n}").solve())
 
 
 if __name__ == '__main__':
