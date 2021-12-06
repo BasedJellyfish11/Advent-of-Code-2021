313891dab92b6e3312657edd0ccfa8c3458f704d
Verulean <65474222+Verulean@users.noreply.github.com>
2021-12-06 09:55:55 -0500

    Day 6

---
+++ Verulean/days/aoc6.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+fmt_dict = {'sep': ','}
+
+def solve(data, day_targets=(80, 256)):
+    initial_population = np.array(data, dtype=int)
+    age_counts = np.array([np.sum(initial_population == i) for i in range(9)], dtype=np.uint64)
+    
+    result = {day:0 for day in day_targets}
+    for day in range(1, max(day_targets)+1):
+        age_counts = np.roll(age_counts, -1)
+        age_counts[6] += age_counts[-1]
+        if day in result:
+            result[day] = np.sum(age_counts)
+
+    return tuple(result[day] for day in day_targets)
