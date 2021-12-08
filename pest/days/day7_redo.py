2d622990ecf2157744bc7a7a26e1a6602fa718da
pest <pestilentbox2@gmail.com>
2021-12-08 02:37:52 -0500

    recursive day 7

--- pest/days/day7_redo.py
+++ pest/days/day7_redo.py
@@ -5,40 +5,81 @@ fileName = "real_inputs7.txt"
 
 import numpy as np
 import sys
+import time
+import math
 
 def part2_fix(tracker):
-    for index, x in enumerate(tracker):
-        tracker[index] = tracker[index]*(tracker[index] + 1)/2
+
+    tracker = tracker * (tracker + 1) // 2
     return tracker
 
 def calc_costs(desired_num, costs, type):
-    e = np.zeros(len(costs))
-    for index, x in enumerate(costs):
-        e[index] = abs(desired_num - x)
+
+    e = np.array(abs(desired_num - costs))
 
     if type==2:
         e = part2_fix(e)
     return e
 
+def time_snapshot():
+    return time.perf_counter()*100
+
+def recurse(index, step_size, data, tracker, max_num, y):
+
+    if abs(step_size) != 1: step_size = math.ceil(step_size/2)
+    # print(f"\tdist being stepped: {step_size}")
+    index += step_size
+
+    cost_left, cost_center, cost_right = (np.dot(tracker, calc_costs(index+x, data, y)) for x in range(-1,2,1))
+    # print(f"{index}:", cost_left - cost_center, 0, cost_right-cost_center, f"step: {step_size}")
+
+    if cost_left > cost_center and cost_right > cost_center: #found min
+        return cost_center
+    elif cost_left > cost_right:
+        return recurse(min(index, max_num), max(step_size, step_size * -1), data, tracker, max_num, y)
+    elif cost_right > cost_left:
+        return recurse(max(index, 0), min(step_size * -1, step_size), data, tracker, max_num, y)
+
+def do_recursion(max_num, data, tracker, y):
+    return recurse(0,max_num+1, data, tracker, max_num, y) #cost of index with minimum steps
+
 def main():
+
+    print("Program Start")
+
+
+    start_time = time_snapshot()
     numbers = read_file(fileName)        # returns list of contents of file as strings
     numbers = np.asarray(list(map(int, numbers[0].split(","))))
+    end_time = time_snapshot()
+    print(f"\ttime elapsed (read in file): {end_time - start_time}ms")
 
     max_num = max(numbers)
     tracker = np.zeros(max_num + 1)
 
-    for x in range(max_num + 1): #propogate inital state
+
+    start_time = time_snapshot()
+    interesting_nums = np.unique(numbers)
+
+    for x in interesting_nums:
         tracker[x] = len(numbers[numbers==x])
 
-    interesting_nums = np.nonzero(tracker)[0]
+
+
+    end_time = time_snapshot()
+    # print(tracker)
+    print(f"\ttime elapsed (propogating tracker): {(end_time - start_time)}ms")
+
     tracker = tracker[tracker != 0]
 
     for y in range(1,3):
-        smallest = sys.maxsize
-        for x in range(max_num + 1):
-            costs = calc_costs(x, interesting_nums, y)
-            a = np.sum(np.multiply(costs, tracker))
-            if a < smallest: smallest = a
-        print(f"smallest ({y}):", smallest)
+        start_time_full = time_snapshot()
 
+        a = do_recursion(max_num, interesting_nums, tracker, y)
+        end_time_full = time_snapshot()
+        print(f"smallest ({y}):", a)
+        print(f"\ttime elapsed: {(end_time_full - start_time_full)}ms")
+start_time = time_snapshot()
 main()
+end_time = time_snapshot()
+print(f"time elapsed (full): {(end_time - start_time)}ms")
