a1db7d907b50f1035330b0220dcb4f145a1ea99e
Verulean <65474222+Verulean@users.noreply.github.com>
2021-12-08 11:03:53 -0500

    Update aoc8.py

--- Verulean/days/aoc8.py
+++ Verulean/days/aoc8.py
@@ -2,14 +2,6 @@ import numpy as np
 from collections import defaultdict
 
 
-def decode_ambiguous(x, discrim1, discrim2, values):
-    if discrim1 < x:
-        return values[0]
-    elif discrim2 < x:
-        return values[1]
-    else:
-        return values[2]
-
 def solve(data):
     # Parse input
     N = len(data)
@@ -22,60 +14,57 @@ def solve(data):
         tests[i] = [set(x) for x in line[0].split()]
         raw_out[i] = [set(x) for x in line[1].split()]
     
-    # Compute linking scheme and decode output numbers
+    # Determine linking scheme and decode output numbers
     maps = defaultdict(lambda: set('X'))
+    uniques = {2: 1, 3: 7, 4: 4, 7: 8}
     for i, (test_seq, num_out) in enumerate(zip(tests, raw_out)):
         maps.clear()
-        fives = []
-        sixes = []
+        ambiguous = []
+        
         for pattern in test_seq:
             n = len(pattern)
-            # 1, 7, 4, 8 have unique counts of lit segments
-            if n == 2:
-                maps[1] = pattern
-            elif n == 3:
-                maps[7] = pattern
-            elif n == 4:
-                maps[4] = pattern
-            elif n == 7:
-                maps[8] = pattern
-            elif n == 5:
-                fives.append(pattern)
-            elif n == 6:
-                sixes.append(pattern)
+            if n in uniques:
+                maps[uniques[n]] = pattern
+            else:
+                ambiguous.append(pattern)
         
-        # Determine the ambiguous numbers using results for 1, 4, 7, 8
-        rune1 = maps[8] - maps[1]
-        #  ####
-        # #    .
-        # #    .
-        #  ####
-        # #    .
-        # #    .
-        #  ####
+        runes = (
+            maps[8] - maps[1],
+            maps[8] - maps[4],
+            maps[4] - maps[7],
+            maps[4] | maps[7],
+            )
         
-        rune2 = maps[8] - maps[4]
-        #  ####
-        # .    .
-        # .    .
-        #  ....
-        # #    .
-        # #    .
-        #  ####
+        # RUNES:
+        #  ####     ####     ....     ####
+        # #    .   .    .   #    .   #    #
+        # #    .   .    .   #    .   #    #
+        #  ####     ....     ####     ####
+        # #    .   #    .   .    .   .    #
+        # #    .   #    .   .    .   .    #
+        #  ####  ,  ####  ,  ....  ,  ....
         
-        for pattern in sixes:
-            maps[decode_ambiguous(pattern, rune1, rune2, (6,0,9))] = pattern
-        
-        for pattern in fives:
-            maps[decode_ambiguous(pattern, rune2, maps[7], (2,3,5))] = pattern
+        # Determine the ambiguous numbers (0,2,3,5,6,9) using known results
+        for pattern in ambiguous:
+            if runes[3] < pattern:
+                n = 9
+            elif runes[0] < pattern:
+                n = 6
+            elif runes[2] < pattern:
+                n = 5
+            elif not runes[1] < pattern:
+                n = 3
+            elif len(pattern) == 5:
+                n = 2
+            else:
+                n = 0
+            maps[n] = pattern
 
         decode = {tuple(sorted(v)):k for k,v in maps.items()}
         dec_out[i] = [decode[tuple(sorted(x))] for x in num_out]
     
     # Part 1
     ans_a = np.sum(np.isin(dec_out, (1,4,7,8)))
-    
     # Part 2
     ans_b = np.dot(np.sum(dec_out, axis=0), np.power([10]*4, [3,2,1,0]))
-    
     return ans_a, ans_b
