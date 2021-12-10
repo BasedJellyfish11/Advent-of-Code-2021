adc4b449b45b2b98d444282ea906157ee49515a8
Ptkyr <48936088+Ptkyr@users.noreply.github.com>
2021-12-10 16:51:17 -0500

    renaming shit

--- Ptkyr/d6input.rkt
+++
@@ -1,8 +0,0 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname d6input) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor mixed-fraction #f #t none #f () #t)))
-(require "provide.rkt")
-(provide input)
-
-(define input '(5 1 5 3 2 2 3 1 1 4 2 4 1 2 1 4 1 1 5 3 5 1 5 3 1 2 4 4 1 1 3 1 1 3 1 1 5 1 5 4 5 4 5 1 3 2 4 3 5 3 5 4 3 1 4 3 1 1 1 4 5 1 1 1 2 1 2 1 1 4 1 4 1 1 3 3 2 2 4 2 1 1 5 3 1 3 1 1 4 3 3 3 1 5 2 3 1 3 1 5 2 2 1 2 1 1 1 3 4 1 1 1 5 4 1 1 1 4 4 2 1 5 4 3 1 2 5 1 1 1 1 2 1 5 5 1 1 1 1 3 1 4 1 3 1 5 1 1 1 5 5 1 4 5 4 5 4 3 3 1 3 1 1 5 5 5 5 1 2 5 4 1 1 1 2 2 1 3 1 1 2 4 2 2 2 1 1 2 2 1 5 2 1 1 2 1 3 1 3 2 2 4 3 1 2 4 5 2 1 4 5 4 2 1 1 1 5 4 1 1 4 1 4 3 1 2 5 2 4 1 1 5 1 5 4 1 1 4 1 1 5 5 1 5 4 2 5 2 5 4 1 1 4 1 2 4 1 2 2 2 1 1 1 5 5 1 2 5 1 3 4 1 1 1 1 5 3 4 1 1 2 1 1 3 5 5 2 3 5 1 1 1 5 4 3 4 2 2 1 3))
-
