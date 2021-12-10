12d85f8fdd5e746fe3b4ccb2a1ac03f7b6c82dff
fifty-six <ybham6@gmail.com>
2021-12-10 00:36:39 -0500

    d10

---
+++ fifty-six/day10/day10.py
@@ -0,0 +1,56 @@
+with open("../input/day10.txt") as f:
+    lines = [x.strip() for x in f.readlines()]
+
+score_p1 = {
+        ')': 3,
+        ']': 57,
+        '}': 1197,
+        '>': 25137
+}
+
+score_p2 = {
+        ')': 1,
+        ']': 2,
+        '}': 3,
+        '>': 4
+}
+
+rev = {
+        ')': '(',
+        '}': '{',
+        ']': '[',
+        '>': '<'
+}
+
+rev_left = { y: x for (x, y) in rev.items() }
+
+left = set(rev.values())
+right = set(rev.keys())
+
+p1_total = 0
+p2_scores = []
+for line in lines:
+    q = []
+    for char in line:
+        if char in left:
+            q.append(char)
+        elif char in right:
+            if q[-1] != rev[char]:
+                p1_total += score_p1[char]
+                break
+            else:
+                q.pop()
+    else:
+        line_score = 0
+
+        while q:
+            c = q.pop()
+            line_score *= 5
+            line_score += score_p2[rev_left[c]]
+
+        p2_scores.append(line_score)
+
+p2_scores.sort()
+
+print("p1: ", total_score_p1)
+print("p2: ", p2_scores[((len(p2_scores) + 1) // 2) - 1])
