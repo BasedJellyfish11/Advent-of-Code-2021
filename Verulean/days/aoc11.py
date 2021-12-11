8bf6ae94bfeca0c92126b56429ccbbc23748c2de
Verulean <65474222+Verulean@users.noreply.github.com>
2021-12-11 01:12:50 -0500

    day 11 refactored solution

--- Verulean/days/aoc11.py
+++ Verulean/days/aoc11.py
@@ -1,52 +1,58 @@
 import numpy as np
 
 
-n_steps = 100
-
-def flash_count(data):
-    return np.count_nonzero(data > 9)
-
-def flashes(data):
-    return np.nonzero(data > 9)
-
-def adjacents(i, j):
-    m, n = 10, 10
-    candidates = ((i-1,j-1),(i-1,j),(i-1,j+1),(i,j-1),(i,j+1),(i+1,j-1),(i+1,j),(i+1,j+1))
+class FlashingOctopuses:
+    def __init__(self, data):
+        self._energy = np.array([list(line) for line in data], dtype=int)
+        self._flashed = np.zeros_like(self._energy, dtype=bool)
+        self._m, self._n = self._energy.shape
+        
+        # Variables for tracking answers
+        self._step_count = 0
+        self._total_flashes = 0
     
-    for x, y in candidates:
-        if 0 <= x < m and 0 <= y < n:
-            yield x, y
+    def flash(self, i, j):
+        # Erroneously, the octopus increases its own energy level as well, but:
+        # 1) Basically guaranteed no entries will overflow so who cares
+        # 2) Slicing this way is easy and it gets set to 0 at the end anyway
+        
+        def bounded_slice(a, b, length):
+            return slice(max(0, a), min(length, b))
+        
+        self._energy[
+            bounded_slice(i-1, i+2, self._m),
+            bounded_slice(j-1, j+2, self._n)
+            ] += 1
+    
+    def step(self):
+        self._step_count += 1
+        self._flashed.fill(False)
+        
+        # Increase energy level of all octopuses
+        self._energy += 1
+        
+        # Process flash propagation
+        prev_flashes = -1 # lol
+        while (curr_flashes := np.count_nonzero(self._flashed)) > prev_flashes:
+            prev_flashes = curr_flashes
+            
+            flash_index = np.nonzero((self._energy > 9) & ~self._flashed)
+            for i, j in zip(*flash_index):
+                self.flash(i, j)
+            
+            self._flashed[flash_index] = True
+        
+        # Reset energy of octopuses that flashed
+        self._energy[self._flashed] = 0
+        
+        self._total_flashes += curr_flashes
+    
+    def solve(self):
+        while not np.all(self._flashed):
+            if self._step_count == 100:
+                hectostep_flashes = self._total_flashes
+            self.step()
+        return hectostep_flashes, self._step_count
 
 def solve(data):
-    data = np.array([list(i) for i in data], dtype=int)
-    flash = np.zeros_like(data, dtype=bool)
-    
-    FLASH_SUM = 0
-    
-    step = 0
-    while np.count_nonzero(flash) != 100:
-        step += 1
-        data += 1
-        
-        flash.fill(False)
-        
-        X = np.count_nonzero(flash)
-        
-        df = 1
-        while df > 0:
-            i_flash = flashes(data)
-            for i, j in zip(*i_flash):
-                if not flash[i,j]:
-                    for i2, j2 in adjacents(i, j):
-                        data[i2, j2] += 1
-            
-            flash[i_flash] = True
-            Y = np.count_nonzero(flash)
-            df, X = Y - X, Y
-        
-        data[flash] = 0
-        FLASH_SUM += np.count_nonzero(flash)
-        
-        if step == 100:
-            ans_a = FLASH_SUM
-    return ans_a, step
+    return FlashingOctopuses(data).solve()
