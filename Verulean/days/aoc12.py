f7c97ca50696dd2f8cc5b1131651893419b210df
Verulean <65474222+Verulean@users.noreply.github.com>
2021-12-12 02:22:15 -0500

    aoc12 refactor

--- Verulean/days/aoc12.py
+++ Verulean/days/aoc12.py
@@ -1,59 +1,59 @@
-import numpy as np
 from collections import defaultdict
 
 
-def all_done(paths):
-    for path in paths:
-        if path[-1] != 'end':
-            return False
-    return True
+class CavePaths:
+    def __init__(self, data):
+        data = [pair.split('-') for pair in data]
+        
+        # Parse data into connection dictionary
+        self._connections = defaultdict(set)
+        for a, b in data:
+            self._connections[a].add(b)
+            self._connections[b].add(a)
+        
+        del self._connections['end']
+        for options in self._connections.values():
+            options -= {'start',}
+        
+        # Intiialize other instance variables
+        self._completed_paths = set()
+        self._small = set(cave for cave in self._connections if cave == cave.lower())
+        
+    def _routes(self, path, small_validator):
+        for cave in self._connections[path[-1]]:
+            if cave in self._small and cave in path:
+                if small_validator(path):
+                    yield cave
+            else:
+                yield cave
+    
+    def _count_paths(self, part):
+        self._completed_paths.clear()
+        
+        if part == 1:
+            def validator(path):
+                return False
+        elif part == 2:
+            def validator(path):
+                for small_cave in self._small:
+                    if path.count(small_cave) >= 2:
+                        return False
+                return True
+        
+        paths = {('start',),}
+        while paths:
+            old_paths, paths = paths, set()
+            for path in old_paths:
+                if path[-1] == 'end':
+                    self._completed_paths.add(path)
+                else:
+                    for next_cave in self._routes(path, validator):
+                        paths.add(path + (next_cave,))
 
-PART = 2
+        return len(self._completed_paths)
+    
+    def solve(self):
+        return tuple(self._count_paths(part) for part in (1, 2))
 
 def solve(data):
-    paths = set()
-    data = [line.split('-') for line in data]
-    
-    data = [(s, e) for s, e in data] \
-        + [(e, s) for s, e in data]
-    
-    ways = defaultdict(list)
-    for start, end in data:
-        if start != 'end' and end != 'start':
-            ways[start] += [end]
-    
-    
-    caves = set(ways.keys())
-    big = set([cave for cave in caves if cave == cave.upper()])
-    
-    small = list(caves - big)
-    
-    def can_fit_extra_small(path):
-        for cave in small:
-            if path.count(cave) > 1:
-                return False
-        return True
-    
-    def options(path):
-        for cave in ways[path[-1]]:
-            if cave in path and cave in small:
-                if PART == 2 and can_fit_extra_small(path):
-                    yield cave
-                continue
-            yield cave
-    
-    
-    paths = {('start',),}
-    
-    while not all_done(paths):
-        new_paths = set()
-        for path in list(paths):
-            if o := list(options(path)):
-                for next_cave in o:
-                    # print(path, next_cave)
-                    new_paths.add(tuple(path + (next_cave,)))
-            elif path[-1] == 'end':
-                new_paths.add(path)
-            paths = new_paths
-    
-    return 'ANSWER', len(paths)
+    return CavePaths(data).solve()
