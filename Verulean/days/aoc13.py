3999b93ccfd246523c1e55fc1a632ee1264f9acd
Verulean <65474222+Verulean@users.noreply.github.com>
2021-12-13 08:52:37 -0500

    day 13 refactor
    operate on discrete points rather than array representation of the transparency
    
    Co-Authored-By: Yusuf Bham <10470872+fifty-six@users.noreply.github.com>

--- Verulean/days/aoc13.py
+++ Verulean/days/aoc13.py
@@ -7,40 +7,35 @@ fmt_dict = {
 
 class TransparencyOrigami:
     def __init__(self, points, folds):
-        points = np.array([coord.split(',') for coord in points], dtype=int)
+        self._points = np.array([coord.split(',') for coord in points], dtype=int)
+        
         folds = [instr[11:].split('=') for instr in folds]
-        
-        n, m = np.amax(points, axis=0) + 1
-        self._paper = np.zeros((m, n), dtype=bool)
-        self._paper[(points[:,1], points[:,0])] = True
-        
         self._folds = tuple(
-            (1 if axis == 'x' else 0, int(index)) 
+            (0 if axis == 'x' else 1, int(index)) 
             for axis, index in folds
             )
     
-    def to_string(self, fg='#', bg=' '):
-        p = np.where(self._paper, fg, bg)
-        return '\n'.join(''.join(row) for row in p)
+    def to_string(self, fg='#', bg=' ', bounds=(40,6)):
+        points = {(x, y) for x, y in self._points}
+        result = ""
+        for y in range(bounds[1]):
+            for x in range(bounds[0]):
+                if (x, y) in points:
+                    result += fg
+                else:
+                    result += bg
+            result += '\n'
+        return result
     
     def _fold(self, axis, index):
-        bottom, _, top = np.split(self._paper, [index, index+1], axis=axis)
-        
-        m_b, n_b = bottom.shape
-        m_t, n_t = top.shape
-        M, N = max(m_b, m_t), max(n_b, n_t)
-        self._paper = np.zeros((M, N), dtype=bool)
-        
-        if axis == 0:
-            self._paper[:m_b] |= bottom
-            self._paper[-1:-(m_t+1):-1] |= top
-        elif axis == 1:
-            self._paper[:, :n_b] |= bottom
-            self._paper[:, -1:-(n_t+1):-1] |= top
+        fold_index = (self._points[:, axis] > index, axis)
+        self._points[fold_index] = 2 * index - self._points[fold_index]
+        # Removing overlapping points here is slow due to sorting/reallocation
+        # self._points = np.unique(self._points, axis=0)
     
     def solve(self):
         self._fold(*self._folds[0])
-        first_fold_dots = np.count_nonzero(self._paper)
+        first_fold_dots = len(self._points)
         
         for axis, index in self._folds[1:]:
             self._fold(axis, index)
