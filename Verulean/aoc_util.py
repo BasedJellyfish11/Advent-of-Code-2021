11114c1b680f065ec8bae27cb90b22df4de4be8f
Verulean <65474222+Verulean@users.noreply.github.com>
2021-12-12 21:31:38 -0500

    Update main/util

--- Verulean/aoc_util.py
+++ Verulean/aoc_util.py
@@ -1,19 +1,20 @@
 from timeit import timeit
-from copy import deepcopy
 import os
 
 
-def aoc_input(file_name, input_dir="input", cast_type=str, strip=True, sep='\n'):
+def aoc_input(file_name, input_dir="input", cast_type=str, strip=True, sep='\n', test=False):
     cwd = os.path.dirname(__file__)
+    if test:
+        file_name = f"test_{file_name}"
     with open(os.path.join(cwd, input_dir, file_name), 'r') as f:
         if sep is None:
             return f.read()
         return [cast_type(i.strip()) if strip else cast_type(i) for i in f.read().split(sep=sep)]
 
-def time_to_string(n, solve, data):
+def time_to_string(n, solve, data, pad=11):
     units = ((1e0, 's'), (1e-3, 'ms'), (1e-6, 'μs'), (1e-9, 'ns'))
-    t = timeit(lambda: solve(deepcopy(data)), number=n) / n
+    t = timeit(lambda: solve(data), number=n) / n
     
     for magnitude, unit in units:
         if t > magnitude:
-            return f"{t/magnitude:.4f} {unit}"
+            return f"{t/magnitude:.4f} {unit}".rjust(pad)
--- Verulean/main.py
+++ Verulean/main.py
@@ -3,17 +3,24 @@ from importlib import import_module
 import aoc_util
 
 
-def main(n=dt.today().day, time=False, n_trials=1000):
+def main(n=dt.today().day, time=False, n_trials=1000, print_solution=True):
     aoc = import_module(f"days.aoc{n:02}")
     fmt = getattr(aoc, 'fmt_dict', {})
     data = aoc_util.aoc_input(f"{n:02}.txt", **fmt)
     
     if time:
         t = aoc_util.time_to_string(n_trials, aoc.solve, data)
-        print(f"Day {n}: average of {t} over {n_trials} runs.")
+        if n_trials == 1:
+            result = t
+        else:
+            result = f"average of {t} over {n_trials} runs."
+        
+        print(f"Day {str(n).rjust(2)}: {result}")
     
     solution = aoc.solve(data)
-    print(solution)
+    if print_solution:
+        print(solution)
+    
     return solution
 
 if __name__ == '__main__':
