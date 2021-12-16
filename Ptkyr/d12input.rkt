67b8d8b07afbe07e816ee7c92ebcd95b6482802a
Ptkyr <48936088+Ptkyr@users.noreply.github.com>
2021-12-15 21:35:50 -0500

    Add files via upload

---
+++ Ptkyr/d12input.rkt
@@ -0,0 +1,52 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname d12input) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor mixed-fraction #f #t none #f () #t)))
+(require "provide.rkt")
+(provide input)
+(provide example)
+
+(define input '(kc-qy
+qy-FN
+kc-ZP
+end-FN
+li-ZP
+yc-start
+end-qy
+yc-ZP
+wx-ZP
+qy-li
+yc-li
+yc-wx
+kc-FN
+FN-li
+li-wx
+kc-wx
+ZP-start
+li-kc
+qy-nv
+ZP-qy
+nv-xr
+wx-start
+end-nv
+kc-nv
+nv-XQ))
+
+
+(define example '(fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW))
