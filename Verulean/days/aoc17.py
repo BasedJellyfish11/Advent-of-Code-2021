2ef7677a10938efc621d1214fa9b301fef2b201a
Verulean <65474222+Verulean@users.noreply.github.com>
2021-12-17 12:36:51 -0500

    day 17 refactor

--- Verulean/days/aoc17.py
+++ Verulean/days/aoc17.py
@@ -1,99 +1,79 @@
-test = True
-
-
-import numpy as np
-from collections import Counter, defaultdict, deque
+from math import floor, ceil, sqrt
 
 fmt_dict = {
-    'sep': '\n'
+    'sep': None
     }
-if test: fmt_dict['file_prefix'] = 'test_'
 
-
-x_range = (185, 221)
-y_range = (-122, -74)
-
-
-# TESTING BOUNDS
-# x_range = (20, 30)
-# y_range = (-10, -5)
-
-def find_n(vx):
-    N = 1
-    while N * vx - (N * (N - 1)) // 2 < 185:
-        N += 1
-    Nu = N
-    while Nu * vx - (Nu * (Nu - 1)) // 2 < 221:
-        Nu += 1
+class TrickShot:
+    def __init__(self, x_bounds, y_bounds):
+        assert(all(x >= 0 for x in x_bounds))
+        
+        self.x_min, self.x_max = x_bounds
+        self.y_min, self.y_max = y_bounds
+        
+        self.vx_min = ceil((-1 + sqrt(1 + 8 * self.x_min)) / 2)
+        self.vy_max = max(abs(self.y_min) - 1, abs(self.y_max))
+        
+        # Answer variables
+        self.style = 0
+        self.shots = 0
     
-    for n in range(N, Nu+1):
-        if (185 <= n*vx - (n*(n-1)) // 2 <= 221):
-            yield n
-
-def find_vy(vx):
-    ns = find_n(vx)
-    for n in ns:
-        yl = -125
-        while n * yl - (n * (n - 1)) // 2 < -122:
-            yl += 1
-        
-        yh = yl
-        while n * yh - (n * (n - 1)) // 2 < -74:
-            yh += 1
-        
-        for vy in range(yl, yh+1):
-            if (-122 <= n * vy - (n*(n-1))//2 <= -74):
-                yield vy
-
-class Probe:
-    def __init__(self, vx, vy):
-        self.x = 0
-        self.y = 0
-        self.vx = vx
-        self.vy = vy
-        self.y_max = 0
+    @staticmethod
+    def x(vx, t):
+        t = min(t, vx + 1)
+        return (-t**2 + t) // 2 + t * vx
     
-    # N steps:
-    # x = N * vx - (N*(N-1))/2
-    # y = N * vy - (N*(N-1))/2
-    def step(self):
-        self.x += self.vx
-        self.y += self.vy
-        
-        if self.vx > 0:
-            self.vx -= 1
-        elif self.vx < 0:
-            self.vx += 1
-        
-        self.vy -= 1
-        
-        self.y_max = max(self.y, self.y_max)
+    @staticmethod
+    def y(vy, t):
+        return (-t**2 + t) // 2 + t * vy
     
-    def ymax(self):
-        prev_ymax = -1
-        while self.y_max > prev_ymax:
-            prev_ymax = self.y_max
-            self.step()
-        return prev_ymax
-        
-# def find_in_bounds(vx):
-#     vy = []
-#     y_maxes = []
-#     for test_vy in range(-100, 100):
-#         p = Probe(vx, test_vy)
-#         while p.check() == 'continue':
-#             p.step()
-#         if p.check() == 'in':
-#             vy.append(test_vy)
-#             y_maxes.append(p.y_max)
-#     return vy, y_maxes
+    @staticmethod
+    def maximum_distance(vx):
+        t = vx + 1
+        return (-t**2 + t) // 2 + t * vx
     
+    @staticmethod
+    def maximum_height(vy):
+        if vy <= 0:
+            return 0
+        return max(map(lambda t: (-t**2 + t) // 2 + t * vy, (vy, vy + 1)))
+    
+    def hits_target(self, vx, vy):
+        peak_x, peak_y = TrickShot.maximum_distance(vx), TrickShot.maximum_height(vy)
+        if peak_x < self.x_min or peak_y < self.y_min:
+            return False
+        
+        def bound_func(trunc, v, target, sign):
+            return trunc(0.5 + v + sign * sqrt((0.5 + v)**2 - 2 * target))
+        
+        t_left = bound_func(ceil, vx, self.x_min, -1)
+        t_bottom = bound_func(floor, vy, self.y_min, 1)
+        
+        t_right = t_bottom \
+            if peak_x <= self.x_max \
+            else bound_func(floor, vx, self.x_max, -1)
+        
+        t_top = t_left \
+            if peak_y <= self.y_max \
+            else bound_func(ceil, vy, self.y_max, 1)
+        
+        return max(t_left, t_top) <= min(t_right, t_bottom)
+    
+    def solve(self):
+        for vx in range(self.vx_min, self.x_max + 1):
+            for vy in range(floor(self.y_min * vx / self.x_min), self.vy_max + 1):
+                if self.hits_target(vx, vy):
+                    self.shots += 1
+                    self.style = max(self.style, TrickShot.maximum_height(vy))
+        
+        return self.style, self.shots
 
 def solve(data):
-    y_max = 0
-    for vx in range(19, 23):
-        for vy in find_vy(vx):
-            y_max = max(y_max, Probe(vx, vy).ymax())
-        # y_max = max(y_max, )
+    data = data.replace("target area: ", '').split(', ')
     
-    return y_max
+    def parse_bounds(line):
+        coords = line.split('=')[1].split('..')
+        return tuple(int(x) for x in coords)
+    
+    x_bounds, y_bounds = map(parse_bounds, data)
+    return TrickShot(x_bounds, y_bounds).solve()
