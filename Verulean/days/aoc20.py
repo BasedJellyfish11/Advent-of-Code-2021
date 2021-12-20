c9688b2a16b13025f07e0d7af12e14ed2cc7e7da
Verulean <65474222+Verulean@users.noreply.github.com>
2021-12-20 00:52:13 -0500

    day 20 refactor
    70/50 on global lb wooooooooooo

--- Verulean/days/aoc20.py
+++ Verulean/days/aoc20.py
@@ -5,37 +5,29 @@ fmt_dict = {
     'sep': '\n\n'
     }
 
-def get_next_char(key, image, i, j):
-    num = ''
-    m, n = image.shape
-    for di in (-1, 0, 1):
-        for dj in (-1, 0, 1):
-            ii = (i+di) % m
-            jj = (j+dj) % n
-            num += str(int(image[ii, jj]))
-    
-    num = int(num, 2)
-    return key[num]
+def step(image, key):
+    image[:] = key[
+          1 * np.roll(np.roll(image, -1, axis=0), -1, axis=1) +
+          2 * np.roll(image, -1, axis=0) +
+          4 * np.roll(np.roll(image, -1, axis=0),  1, axis=1) +
+          8 * np.roll(image, -1, axis=1) +
+         16 * image +
+         32 * np.roll(image,  1, axis=1) +
+         64 * np.roll(np.roll(image,  1, axis=0), -1, axis=1) +
+        128 * np.roll(image,  1, axis=0) +
+        256 * np.roll(np.roll(image,  1, axis=0),  1, axis=1)
+        ]
 
 def solve(data, N=50):
     key = np.array(list(data[0]))
     key = np.where(key == '#', 1, 0)
     
-    im0 = [list(line) for line in data[1].split('\n')]
-    im0 = np.array(im0)
-    im0 = np.where(im0 == '#', 1, 0)
-    im0 = np.pad(im0, N)
+    image = np.array([list(line) for line in data[1].split('\n')])
+    image = np.pad(np.where(image == '#', 1, 0), N)
     
-    m, n = im0.shape
-    
-    for step in range(1,N+1):
-        next_im = np.copy(im0)
-        for i in range(m):
-            for j in range(n):
-                next_im[i, j] = get_next_char(key, im0, i, j)
-        im0 = next_im
-        
-        if step == 2:
-            ans_a = np.count_nonzero(next_im)
+    for iteration in range(1, N+1):
+        step(image, key)
+        if iteration == 2:
+            ans_a = np.count_nonzero(image)
 
-    return ans_a, np.count_nonzero(im0)
+    return ans_a, np.count_nonzero(image)
