8d99c76ac9431da0114fbd14bbe0bd56d960e36f
PaumIsMe <theapologeticweasels@gmail.com>
2021-12-22 16:10:18 -0500

    Paum but I'm actually really proud of 22 ^w^

---
+++ paum/21/21.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+results = [(3, 1), (4, 3), (5, 6), (6, 7), (7, 6), (8, 3), (9, 1)]
+
+def simulate(score1, score2, pos1, pos2, num_universes, turn):
+
+    #print(f"{score1, score2, pos1, pos2, num_universes, turn}")
+
+    if score1 >= 21:
+        return np.array([num_universes, 0])
+    elif score2 >= 21:
+        return np.array([0, num_universes])
+    
+    ret = np.zeros(2)
+    for result in results:
+        s1_new = score1
+        s2_new = score2
+        pos1_new = pos1
+        pos2_new = pos2
+        if (turn == 0):
+            pos1_new = (pos1_new + result[0]) % 10
+            s1_new += 10 if pos1_new == 0 else pos1_new
+        else:
+            pos2_new = (pos2_new + result[0]) % 10
+            s2_new += 10 if pos2_new == 0 else pos2_new    
+
+        ret = ret + simulate(s1_new, s2_new, pos1_new, pos2_new, num_universes * result[1], (turn + 1) % 2)
+    
+    return ret
+
+def main():
+
+    with open("./input.txt") as f:
+        pos = [int(line.split()[-1]) for line in f.readlines()]
+
+    pos2 = [pos[0], pos[1]]
+    score = [0, 0]
+    print(pos)
+
+    turn = 0
+
+    #Part 1
+
+    for i in range(1, 10000, 3):
+
+        total_roll = (i % 100) + ((i + 1) % 100) + ((i + 2) % 100)
+        pos[turn] = (pos[turn] + total_roll) % 10
+        score[turn] += 10 if pos[turn] == 0 else pos[turn]
+        print(score[turn])
+        if (score[turn] >= 1000):
+            break
+        turn = (turn + 1) % 2
+
+    print(f"Part 1: {(i + 2) * min(score[0], score[1])}")
+
+    #Part 2 recursively cause I'm lazy but dynamic would be interesting so hopefully veru does that he's pretty cool
+
+    my_ret = simulate(0, 0, pos2[0], pos2[1], 1, 0)
+    print(f"{max(my_ret[0], my_ret[1]):.20f}")
+
+if __name__ == "__main__":
+    main()
---
+++ paum/22/22.py
@@ -0,0 +1,95 @@
+import numpy as np
+
+class area:
+    
+    def __init__(self, x_coords, y_coords, z_coords, parity):
+        self.x_coords = x_coords
+        self.y_coords = y_coords
+        self.z_coords = z_coords
+        self.overlaps = []
+        self.parity = parity
+
+    def overlapping(self, a, b):
+        return a[1] > b[0] and b[1] > a[0]
+    
+    def add_overlap(self, new_area):
+        if self.overlapping(self.x_coords, new_area.x_coords) and self.overlapping(self.y_coords, new_area.y_coords) and self.overlapping(self.z_coords, new_area.z_coords):
+            x = (max(self.x_coords[0], new_area.x_coords[0]), min(self.x_coords[1], new_area.x_coords[1]))
+            y = (max(self.y_coords[0], new_area.y_coords[0]), min(self.y_coords[1], new_area.y_coords[1]))
+            z = (max(self.z_coords[0], new_area.z_coords[0]), min(self.z_coords[1], new_area.z_coords[1]))
+            self.overlaps.append(area(x, y, z, 1))
+    
+    def get_parity(self):
+        return self.parity
+
+    def dist(self, i):
+        return i[1] - i[0]
+
+    def get_box_area(self):
+        return self.dist(self.x_coords) * self.dist(self.y_coords) * self.dist(self.z_coords)
+
+    def __str__(self):
+        my_str = f"x: {self.x_coords} y: {self.y_coords} z: {self.z_coords}"
+        for a in self.overlaps:
+            my_str += f"\n\t{a}"
+        return my_str
+
+def get_instructions():
+
+    with open("./input.txt") as f:
+        instructions = [line.strip() for line in f.readlines()]
+
+    for i, line in enumerate(instructions):
+        onoff, input_string = line.split(' ')
+        onoff = 0 if onoff == 'off' else 1
+        arr = [int(num) for num in input_string.replace('x=', '').replace('y=', '').replace('z=', '').replace('..',',').split(',')]
+        instructions[i] = (onoff, arr)
+
+    return instructions
+
+def dist(x):
+    return x[1] - x[0]
+    
+def get_total_area(area_list):
+
+    #print("Getting Total Area of List")
+
+    if len(area_list) == 0:
+        return 0
+    
+    elif len(area_list) == 1:
+        a = area_list[0]
+
+    areas = []
+
+    for a in area_list:
+        for established_area in areas:
+            established_area.add_overlap(a)
+        if a.get_parity():
+            areas.append(a)
+    
+    total_area = 0
+
+    for a in areas:
+        total_area += a.get_box_area()
+        total_area -= get_total_area(a.overlaps)
+
+    return total_area
+    
+
+def main():
+
+    instructions = get_instructions()
+    area_list = [area((inst[0], inst[1] + 1), (inst[2], inst[3] + 1), (inst[4], inst[5] + 1), par) for par, inst in instructions]
+    tot_a = get_total_area(area_list[0:20])
+
+    instructions = get_instructions()
+    area_list = [area((inst[0], inst[1] + 1), (inst[2], inst[3] + 1), (inst[4], inst[5] + 1), par) for par, inst in instructions]
+    tot_b = get_total_area(area_list)
+
+    print(f"Part A: {tot_a}")
+    print(f"Part B: {tot_b}")
+    
+
+if __name__ == "__main__":
+    main()
